4b04312856ae11ee6acedfde749d1b7c470a7021
Jadir Moussa <moussa1730@gmail.com>
2021-01-08 01:29:20 +0100

    add select lang of navMenu

--- src/components/layouts/lang.vue
+++ src/components/layouts/lang.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="locale-changer">
-        <select v-model="$i18n.locale">
-            <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">{{ lang }}</option>
-        </select>
+        <el-select v-model="$i18n.locale" placeholder="lang">
+            <el-option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">{{ lang }}</el-option>
+        </el-select>
     </div>
 </template>
 
